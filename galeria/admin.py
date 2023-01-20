81ebd9c2a10c2a3bc2d9d635f2d57b161b549455
victormed77 <victormedeiros1199@gmail.com>
2023-01-20 11:29:52 -0400

    segundo commit alterações

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'categoria', 'publicada')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_per_page = 10
+    list_editable = ('publicada',)
+
+
+admin.site.register(Fotografia, ListandoFotografia)
