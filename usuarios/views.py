a5863336fa5609115eb08b8f8b7491d1135405fc
victormed77 <victormedeiros1199@gmail.com>
2023-01-25 17:47:07 -0400

    adcionando templates/forms dos usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from usuarios.forms import LoginForms, CadastroForms
+
+
+def login(request):
+    form = LoginForms()
+    return render(request, "usuarios/login.html", {"form": form})
+
+
+def cadastro(request):
+    form = CadastroForms()
+    return render(request, "usuarios/cadastro.html", {"form": form})
