25cba6773d8849726631694e565581630cfba07e
victormed77 <victormedeiros1199@gmail.com>
2023-02-01 12:07:11 -0400

    alertas e mensagens

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,7 +4,7 @@ from usuarios.forms import LoginForms, CadastroForms
 
 from django.contrib.auth.models import User
 
-from django.contrib import auth
+from django.contrib import auth, messages
 
 
 def login(request):
@@ -25,8 +25,10 @@ def login(request):
 
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f"{nome} logado com sucesso")
             return redirect("index")
         else:
+            messages.error(request, "Erro ao efetuar login")
             return redirect("login")
 
     return render(request, "usuarios/login.html", {"form": form})
@@ -40,6 +42,7 @@ def cadastro(request):
 
         if form.is_valid():
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, "Senhas não são iguais")
                 return redirect('cadastro')
 
             nome = form["nome_cadastro"].value()
@@ -47,6 +50,7 @@ def cadastro(request):
             senha = form["senha_1"].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuário já existente")
                 return redirect('cadastro')
 
             usuario = User.objects.create_user(
@@ -56,6 +60,7 @@ def cadastro(request):
             )
 
             usuario.save()
+            messages.success(request, "Cadastro efetuado com sucesso")
             return redirect('login')
 
     return render(request, "usuarios/cadastro.html", {"form": form})
