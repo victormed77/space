3c720c01685cd93f4aceaf0bd7aa0857f1a6d433
victormed77 <victormedeiros1199@gmail.com>
2023-02-03 15:48:06 -0400

    Associando Tabelas

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,7 +6,7 @@ class ListandoFotografia(admin.ModelAdmin):
     list_display = ('id', 'nome', 'legenda', 'categoria', 'publicada')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
-    list_filter = ('categoria',)
+    list_filter = ('categoria', 'usuario')
     list_per_page = 10
     list_editable = ('publicada',)
 
