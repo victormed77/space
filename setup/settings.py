d4a5dfa47a9de3c19269076ce1c58e11f3d6ae9e
victormed77 <victormedeiros1199@gmail.com>
2023-02-08 11:52:04 -0400

    Partial para alertas

--- setup/settings.py
+++ setup/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path, os
 from dotenv import load_dotenv
 
@@ -138,3 +139,9 @@ MEIDA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
